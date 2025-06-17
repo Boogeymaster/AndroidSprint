d458583b56b9a9413a27a6d6ae1f9b502e8f49a0
Boogeymaster <dr.smuuurge@gmail.com>
2025-06-17 21:50:42 +0300

    RA-04 add-fragment.ktx-and-CategoriesListFragment

---
+++ app/src/main/java/com/example/androidsprint/CategoriesListFragment.kt
@@ -0,0 +1,27 @@
+package com.example.androidsprint
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.androidsprint.databinding.FragmentListCategoriesBinding
+
+class CategoriesListFragment : Fragment() {
+    private var _binding: FragmentListCategoriesBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("binding for CategoriesListFragment is null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+
+}
--- app/src/main/java/com/example/androidsprint/MainActivity.kt
+++ app/src/main/java/com/example/androidsprint/MainActivity.kt
@@ -2,23 +2,23 @@ package com.example.androidsprint
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.androidsprint.databinding.ActivityMainBinding
-import com.example.androidsprint.ui.theme.AndroidSprintTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         setContentView(binding.root)
     }
 }
